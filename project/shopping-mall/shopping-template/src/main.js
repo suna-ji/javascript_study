eb5c2954bc6e694571c350ff8f3cb4f55d127516
지선아 <jisuna314@gmail.com>
2020-12-26 22:40:27 +0900

    item list

---
+++ project/shopping-mall/shopping-template/src/main.js
@@ -0,0 +1,10 @@
+var clothes = [
+    { img : "../../../img/pink_t.png", category = "tshirt", color: "pink", gender : "female", size = "large"},
+    { img : "../../../img/pink_t.png", category = "tshirt", color: "pink", gender : "male", size = "small"},
+    { img : "../../../img/pink_p.png", category = "pants", color: "pink",gender : "male", size = "small"},
+    { img : "../../../img/yellow_p.png", category = "pants", color: "yellow", gender : "female", size = "large"},
+]
+
+var clickButton = (keyword) => {
+    
+}
