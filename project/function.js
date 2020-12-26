43d7b87900af3fc33c19ec32bd6e6060735f5cc9
지선아 <jisuna314@gmail.com>
2020-12-27 03:53:05 +0900

    item list

--- project/function.js
+++ project/function.js
@@ -10,12 +10,12 @@
 // e.g. (exempli gratia 예를들면) createCardAndPoint -> createCard, createProject
 // e.i. (id est 다시말해서)
 // function is object in JS
-function printHello(){
+function printHello() {
     console.log('Hello');
 }
 printHello();
 
-function log(message){
+function log(message) {
     console.log(message);
 }
 log('hihi!');
@@ -24,27 +24,27 @@ log(122);
 // 2. Parameters
 // primitive parameters: passed by value
 // object parameters: passed by reference
-function changeName(obj){
+function changeName(obj) {
     obj.name = 'coder';
 }
-const ellie = {name: 'ellie'};
+const ellie = { name: 'ellie' };
 changeName(ellie);
 console.log(ellie); // ellie -> changeName -> coder
 
 // 3. Default parameters (added in ES6)
-function showMessage(message, from = 'unkown'){
+function showMessage(message, from = 'unkown') {
     console.log(`${message} from ${from}`);
 }
 showMessage("hi");
 showMessage("hi", "ellie");
 
 // 4. Rest parameters (added in ES6)
-function printAll(...args){
-    for(let i = 0; i < args.length; i++){
+function printAll(...args) {
+    for (let i = 0; i < args.length; i++) {
         console.log(args[i]);
     }
     // 또는
-    for(const arg of args){
+    for (const arg of args) {
         console.log(arg);
     }
     // 또는
@@ -54,12 +54,12 @@ printAll('a', 'b', 'c');
 
 // 5. Local scope
 let globalMessage = 'global'; // global variable
-function printMessage(){
+function printMessage() {
     let message = 'hello'; // local variable
     console.log(message);
     console.log(globalMessage);
 
-    function printAnother(){
+    function printAnother() {
         console.log(message);
         let childMessage = 'hello';
     }
@@ -68,7 +68,7 @@ function printMessage(){
 printMessage();
 
 // 6. Return a value
-function sum(a, b){
+function sum(a, b) {
     return a + b;
 }
 const result = sum(1, 2);
@@ -76,15 +76,15 @@ console.log(`sum : ${sum(1, 2)}`);
 
 // 7. Early return, early exit
 // bad
-function upgradeUser(user){
-    if(user.point > 10){
+function upgradeUser(user) {
+    if (user.point > 10) {
         // long upgrade logic...
     }
 }
 
 // good
-function upgradeUser(user){
-    if(user.point <= 10){
+function upgradeUser(user) {
+    if (user.point <= 10) {
         exit;
     }
     // long upgrade logic...
@@ -99,7 +99,7 @@ function upgradeUser(user){
 // 1. Function expression
 // a function declaration can be called earlier than it is defined (hoisted)
 // a function expression is created when the execution reaches it
-const print = function(){
+const print = function () {
     // anonymous function
     console.log('print');
 }; // assinged as a value to variable
@@ -107,22 +107,22 @@ print();
 const printAgain = print; //  assigned as a value to variable
 printAgain();
 const sumAgain = sum;
-console.log(sumAgain(1, 3)); 
+console.log(sumAgain(1, 3));
 
 // 2. callback function using function expression
-function randomQuiz(answer, printYes, printNo){ // passed as an argument to other functions.
-    if(answer == 'love you'){
+function randomQuiz(answer, printYes, printNo) { // passed as an argument to other functions.
+    if (answer == 'love you') {
         printYes(); // hoisted
-    }else{
+    } else {
         printNo(); // hoisted
     }
 }
 // anonymous funciton
-const printYes = function(){
+const printYes = function () {
     console.log('yes');
 };
 
-const printNo = function(){
+const printNo = function () {
     console.log('no');
 };
 
@@ -136,16 +136,21 @@ randomQuiz('love you', printYes, printNo);
 // const simplePrint = function(){
 //    console.log('simplePrint!');
 // };
+console.clear();
 const simplePrint = () => console.log('simplePrint!');
-const add = (a, b) => a + b;
-const simpleMultiply = (a, b) =>{
+const add = (a, b) => { return a + b; }
+// const add = (a, b) => a + b;
+const simpleMultiply = (a, b) => {
     // do something more 
     return a * b;
 };
+const sum_result = add(1, 3);
+console.log(sum_result);
+
 
 // IIFE : Immediately Invoked Function Expression (바로 실행)
 (
-    function hello(){
+    function hello() {
         console.log('IIFE');
     }
 )();
@@ -155,8 +160,8 @@ const simpleMultiply = (a, b) =>{
 // command: add, substract, divide, multiply, remainder
 
 // 1. named function
-function calculate1(command, a, b){
-    switch(command){
+function calculate1(command, a, b) {
+    switch (command) {
         case 'add':
             console.log(`a + b = ${a + b}`);
             break;
@@ -177,8 +182,8 @@ function calculate1(command, a, b){
     }
 }
 // 2. Arrow function
-const calculate2 = (command, a, b) =>{
-    switch(command){
+const calculate2 = (command, a, b) => {
+    switch (command) {
         case 'add':
             console.log(`a + b = ${a + b}`);
             break;
--- project/shopping-mall/shopping-template/src/main.js
+++ project/shopping-mall/shopping-template/src/main.js
@@ -6,6 +6,33 @@
 //     { img: "", category = "pants", color: "yellow", gender: "female", size = "large" },
 // ]
 
-var clickButton = (keyword) => {
-
+// Fetch the items from the JSON file
+function loadItems() {
+    return fetch('data/data.json')
+        .then(response => response.json())
+        .then(json => json.items)
 }
+// Update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+    return `
+        <li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+            <span class="item__description">${item.gender}, ${item.size}</span>
+        </li>
+    `;
+}
+
+// json파일을 동적으로 불러와야함 -> 시간이 걸림 -> promise리턴
+// main
+loadItems()
+    .then(items => {
+        displayItems(items);
+        //setEventListeners();
+    })
+    .catch(console.log);
